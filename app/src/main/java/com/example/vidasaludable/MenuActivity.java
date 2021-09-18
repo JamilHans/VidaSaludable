297ec6504fc792bab187f822254639d2d54488b6
JamilHans <jamilhfc16@gmail.com>
2021-09-17 20:03:42 -0500

    First Update IMC

--- app/src/main/java/com/example/vidasaludable/MenuActivity.java
+++ app/src/main/java/com/example/vidasaludable/MenuActivity.java
@@ -2,13 +2,30 @@ package com.example.vidasaludable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.vidasaludable.control.ControlActivity;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button btnControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        btnControl = (Button) findViewById(R.id.btnControl);
+
+        btnControl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MenuActivity.this, ControlActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
