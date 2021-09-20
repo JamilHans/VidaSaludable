a2a31d5317cab2f7922c90c8456a7ecf5e9f0bdb
JamilHans <jamilhfc16@gmail.com>
2021-09-19 22:53:51 -0500

    update button

--- app/src/main/java/com/example/vidasaludable/control/ControlActivity.java
+++ app/src/main/java/com/example/vidasaludable/control/ControlActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 import com.example.vidasaludable.R;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class ControlActivity extends AppCompatActivity {
 
     private TextInputLayout textFieldPeso;
+    private MaterialButton textButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
