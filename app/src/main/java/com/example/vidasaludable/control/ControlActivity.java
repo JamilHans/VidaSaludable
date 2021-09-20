701fb9913f1c12b99529e98624f9c68eef6f71cb
Jhonrg <jhonatanrg03@gmail.com>
2021-09-19 22:38:06 -0500

    Test commit

--- app/src/main/java/com/example/vidasaludable/control/ControlActivity.java
+++ app/src/main/java/com/example/vidasaludable/control/ControlActivity.java
@@ -3,14 +3,25 @@ package com.example.vidasaludable.control;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.vidasaludable.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class ControlActivity extends AppCompatActivity {
+
+    private TextInputLayout textFieldPeso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
+
+        textFieldPeso = findViewById(R.id.textFieldPeso);
+
+
+
+
     }
 }
